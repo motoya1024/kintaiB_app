45ce528c8944199d0a0e304e219bfa3ee4ceee7c
Motoya Abe <abetty9750@yahoo.co.jp>
2018-11-03 07:41:29 +0000

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,14 @@ class ApplicationController < ActionController::Base
     render html: "hello, world!"
   end
   
+  private
+   # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
   
 end
