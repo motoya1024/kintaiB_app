ba59fe4a99ec606d52a6a5dedc45aa135cfe3a65
Motoya Abe <abetty9750@yahoo.co.jp>
2018-11-04 02:10:01 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,14 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :users
+   resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
