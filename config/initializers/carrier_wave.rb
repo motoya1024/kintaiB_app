eecbff4a9fe31455ee80b58b914b6a44360cbbc1
motoyaabe <abetty9750@gmail.com>
2018-12-22 03:27:03 +0000

    delete uploader carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+=begin if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Amazon S3用の設定
@@ -9,4 +9,4 @@ if Rails.env.production?
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
-end
+=end end
